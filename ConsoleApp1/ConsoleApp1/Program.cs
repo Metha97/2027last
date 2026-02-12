cd1d4435bd1c4c4aa8ca81445286d83b6c05f89d
Metha97 <metha.methapohn@gmail.com>
2026-02-12 15:04:42 +0900

    integer or not

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.Write("Enter a Number : ");
+        string input = Console.ReadLine();
+        int number;
+        if(int.TryParse(input, out number))
+        {
+            Console.WriteLine("integer");
+        }
+        else
+        {
+            Console.WriteLine("not integer");
+        }
+        Console.ReadLine();
+    }
+}
